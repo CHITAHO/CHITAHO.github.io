4e54d1bc9efcee364098961f0574cb5d449b6292
thchoi <thchoi@mindone.co.kr>
2023-03-28 16:19:41 +0900

    reactBlog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
+import Home from './pages/Home';
+import Java from './pages/Java';
+import JavaScript from './pages/JavaScript';
+import NodeJS from './pages/NodeJS';
+import ReactJS from './pages/ReactJS';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav className="menu">
+          <ul>
+            <li><Link to="/Home">Home</Link></li>
+            <li><Link to="/java">Java</Link></li>
+            <li><Link to="/javascript">JavaScript</Link></li>
+            <li><Link to="/nodejs">NodeJS</Link></li>
+            <li><Link to="/reactjs">ReactJS</Link></li>
+          </ul>
+        </nav>
+        <header>
+          <h1>개발 블로그 페이지</h1>
+        </header>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/java" element={<Java />} />
+          <Route path="/javascript" element={<JavaScript />} />
+          <Route path="/nodejs" element={<NodeJS />} />
+          <Route path="/reactjs" element={<ReactJS />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
