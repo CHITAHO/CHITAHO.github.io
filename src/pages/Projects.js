529d6351c1a92d0ce0240383a0830a12e8cd48c8
thchoi <thchoi@mindone.co.kr>
2023-04-05 14:03:55 +0900

    프로젝트 화면 이미지 제거

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,10 +7,10 @@ function Projects() {
     <div>
         <h2>AI 공공하수처리시설 지능화 시스템</h2>
         <p>기술스택 : Java, SpringBoot, JavaScript, NodeJS, PostgreSQL</p>
-        <img src={AI_Intelligent_SW} alt="AI_Intelligent_SW 이미지" style={{ width: '1500px', height: 'auto' }} />
+        {/* <img src={AI_Intelligent_SW} alt="AI_Intelligent_SW 이미지" style={{ width: '1500px', height: 'auto' }} /> */}
         <h2>상수관망 최적관리 시스템(Uni-EM-Water 2.0 Professional)</h2>
         <p>기술 스택 : Java, Spring, JavaScript, Oracle </p>
-        <img src={Uni_EM_Water_2} alt="Uni_EM_Water_2 이미지" style={{ width: '1500px', height: 'auto' }} />
+        {/* <img src={Uni_EM_Water_2} alt="Uni_EM_Water_2 이미지" style={{ width: '1500px', height: 'auto' }} /> */}
     </div>
     
   );
